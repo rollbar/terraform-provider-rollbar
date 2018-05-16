3f78a28b657b4f29cc278d34a339e68815a0c2e5
parabolic <nvelkovski@gmail.com>
2018-05-16 11:59:26 +0200

    fix the variable names

--- users_test.go
+++ users_test.go
@@ -4,32 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"testing"
 )
 
-const (
-	team_id    = "2131231"
-	user_id    = "1"
-	user_email = "brian@rollbar.com"
-)
-
-var (
-	mux    *http.ServeMux
-	server *httptest.Server
-	client *Client
-)
-
-func setup() func() {
-	mux = http.NewServeMux()
-	server = httptest.NewServer(mux)
-	client, _ = NewClient(ApiKey("mock_api_key"), BaseURL(server.URL+"/"))
-
-	return func() {
-		server.Close()
-	}
-}
 func fixture(path string) string {
 	b, err := ioutil.ReadFile("testdata/fixtures/" + path)
 	if err != nil {
@@ -41,23 +19,23 @@ func fixture(path string) string {
 func TestRemoveUserTeam(t *testing.T) {
 	teardown := setup()
 	defer teardown()
-	handler_url := "/team/" + team_id + "/user/" + user_id
-	handler_url_get := "/users/"
+	handlerUrl := fmt.Sprintf("/team/%s/user/%s", teamID, userID)
+	handlerUrlGet := "/users/"
 
-	mux.HandleFunc(handler_url, func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc(handlerUrl, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, fixture("users/remove_user.json"))
 	})
 
-	mux.HandleFunc(handler_url_get, func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc(handlerUrlGet, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, fixture("users/users.json"))
 	})
 
-	teamtoi, _ := strconv.Atoi(team_id)
-	err := client.RemoveUserTeam(user_email, teamtoi)
+	teamTOI, _ := strconv.Atoi(teamID)
+	err := client.RemoveUserTeam(userEmail, teamTOI)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -66,15 +44,15 @@ func TestRemoveUserTeam(t *testing.T) {
 func TestInviteUser(t *testing.T) {
 	teardown := setup()
 	defer teardown()
-	handler_url := "/team/" + team_id + "/invites"
+	handlerUrl := fmt.Sprintf("/team/%s/invites", teamID)
 
-	mux.HandleFunc(handler_url, func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc(handlerUrl, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, fixture("teams/invite_user.json"))
 	})
-	teamtoi, _ := strconv.Atoi(team_id)
-	_, err := client.InviteUser(teamtoi, user_email)
+	teamTOI, _ := strconv.Atoi(teamID)
+	_, err := client.InviteUser(teamTOI, userEmail)
 
 	if err != nil {
 		t.Fatal(err)
@@ -84,8 +62,9 @@ func TestInviteUser(t *testing.T) {
 func TestListUsers(t *testing.T) {
 	teardown := setup()
 	defer teardown()
+	handlerUrl := "/users"
 
-	mux.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc(handlerUrl, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, fixture("users/users.json"))
@@ -99,17 +78,18 @@ func TestListUsers(t *testing.T) {
 func TestGetId(t *testing.T) {
 	teardown := setup()
 	defer teardown()
-	mux.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+	handlerUrl := "/users"
+	mux.HandleFunc(handlerUrl, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, fixture("users/users.json"))
 	})
 	// Email is taken from the ./testdata/fixtures/users/users.json
-	user_id, err := client.getId(user_email)
+	userID, err := client.getId(userEmail)
 
 	if err != nil {
 		t.Fatal(err)
 	}
 	// This will fail if we get anything rather than a number
-	strconv.Itoa(user_id)
+	strconv.Itoa(userID)
 }
