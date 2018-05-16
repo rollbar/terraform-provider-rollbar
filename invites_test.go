2350216fef7d9268a887a67232fda773c45448bc
parabolic <nvelkovski@gmail.com>
2018-05-16 11:53:30 +0200

    fix the variables, add a logic for pagination simulating the api behaviour

--- invites_test.go
+++ invites_test.go
@@ -4,25 +4,34 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 	"testing"
 )
 
 func TestListInvites(t *testing.T) {
 	teardown := setup()
 	defer teardown()
-	handler_url := "/team/" + team_id + "/invites/"
+	handUrl := fmt.Sprintf("/team/%s/invites/", teamID)
 
-	mux.HandleFunc(handler_url, func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc(handUrl, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, fixture("teams/invites.json"))
+
+		// Check if we've hit the second page and return an empty paginated
+		// response thus simulating the apis behaviour.
+		splstr := strings.Split(r.URL.RawQuery, "&")
+		if splstr[1] == "page=2" {
+			fmt.Fprint(w, fixture("teams/empty_invites.json"))
+		} else {
+			fmt.Fprint(w, fixture("teams/invites.json"))
+		}
 	})
 
-	teamtoi, _ := strconv.Atoi(team_id)
-	_, err := client.ListInvites(teamtoi)
+	// team_id should e int for this particular func
+	teamTOI, _ := strconv.Atoi(teamID)
+	_, err := client.ListInvites(teamTOI)
 
 	if err != nil {
 		t.Fatal(err)
 	}
-
 }
