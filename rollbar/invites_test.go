c178493b93f6e4a2547bc520705507942933c7be
parabolic <nvelkovski@gmail.com>
2018-05-17 11:01:31 +0200

    use a better way for defining the fictures

--- rollbar/invites_test.go
+++ rollbar/invites_test.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
 	"testing"
 )
 
 func TestListInvites(t *testing.T) {
+	var fixtureName string
 	teardown := setup()
 	defer teardown()
 	handUrl := fmt.Sprintf("/team/%s/invites/", teamID)
@@ -16,15 +16,16 @@ func TestListInvites(t *testing.T) {
 	mux.HandleFunc(handUrl, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
+		page := r.URL.Query().Get("pages")
 
-		// Check if we've hit the second page and return an empty paginated
-		// response thus simulating the apis behaviour.
-		splStr := strings.Split(r.URL.RawQuery, "&")
-		if splStr[1] == "page=2" {
-			fmt.Fprint(w, fixture("teams/empty_invites.json"))
+		if page == "0" || page == "1" {
+			fixtureName = "invites"
 		} else {
-			fmt.Fprint(w, fixture("teams/invites.json"))
+			fixtureName = "empty_invites"
 		}
+
+		fmt.Fprint(w, fixture(fmt.Sprintf("teams/%s.json", fixtureName)))
+
 	})
 
 	// team_id should e int for this particular func
