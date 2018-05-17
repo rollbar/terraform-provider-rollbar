8bf038b2c1096cf920d539e88674325ac9da032b
parabolic <nvelkovski@gmail.com>
2018-05-17 15:49:10 +0200

    use the vars function only for the test methods that need it. Same goes with the variables

--- rollbar/users_test.go
+++ rollbar/users_test.go
@@ -10,7 +10,18 @@ import (
 func TestRemoveUserTeam(t *testing.T) {
 	teardown := setup()
 	defer teardown()
-	handUrl := fmt.Sprintf("/team/%s/user/%s", teamID, userID)
+
+	vars, err := vars("users.json")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	teamID := vars.TeamID
+	userID := vars.UserID
+	userEmail := vars.UserEmail
+
+	handUrl := fmt.Sprintf("/team/%d/user/%d", teamID, userID)
 	handUrlGet := "/users/"
 
 	mux.HandleFunc(handUrl, func(w http.ResponseWriter, r *http.Request) {
@@ -25,8 +36,7 @@ func TestRemoveUserTeam(t *testing.T) {
 		fmt.Fprint(w, fixture("users/users.json"))
 	})
 
-	teamTOI, _ := strconv.Atoi(teamID)
-	err := client.RemoveUserTeam(userEmail, teamTOI)
+	err = client.RemoveUserTeam(userEmail, teamID)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -35,15 +45,23 @@ func TestRemoveUserTeam(t *testing.T) {
 func TestInviteUser(t *testing.T) {
 	teardown := setup()
 	defer teardown()
-	handUrl := fmt.Sprintf("/team/%s/invites", teamID)
+
+	vars, err := vars("users.json")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	teamID := vars.TeamID
+	userEmail := vars.UserEmail
+	handUrl := fmt.Sprintf("/team/%d/invites", teamID)
 
 	mux.HandleFunc(handUrl, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, fixture("teams/invite_user.json"))
 	})
-	teamTOI, _ := strconv.Atoi(teamID)
-	_, err := client.InviteUser(teamTOI, userEmail)
+	_, err = client.InviteUser(teamID, userEmail)
 
 	if err != nil {
 		t.Fatal(err)
@@ -53,6 +71,7 @@ func TestInviteUser(t *testing.T) {
 func TestListUsers(t *testing.T) {
 	teardown := setup()
 	defer teardown()
+
 	handUrl := "/users"
 
 	mux.HandleFunc(handUrl, func(w http.ResponseWriter, r *http.Request) {
@@ -60,7 +79,9 @@ func TestListUsers(t *testing.T) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, fixture("users/users.json"))
 	})
+
 	_, err := client.ListUsers()
+
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -69,18 +90,27 @@ func TestListUsers(t *testing.T) {
 func TestGetId(t *testing.T) {
 	teardown := setup()
 	defer teardown()
+
+	vars, err := vars("users.json")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	userID := vars.UserID
+	userEmail := vars.UserEmail
 	handUrl := "/users"
+
 	mux.HandleFunc(handUrl, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, fixture("users/users.json"))
 	})
-	// Email is taken from the ./testdata/fixtures/users/users.json
-	userID, err := client.getID(userEmail)
+	userID, err = client.getID(userEmail)
 
 	if err != nil {
 		t.Fatal(err)
 	}
-	// This will fail if we get anything rather than a number
+	// This will fail if we get anything rather than a int.
 	strconv.Itoa(userID)
 }
