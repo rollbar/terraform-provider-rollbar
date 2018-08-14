e2f3d8d3c5e97bb705ab10ea8c2b9fc92d9772bb
parabolic <nvelkovski@gmail.com>
2018-08-14 12:53:10 +0200

    the terraform provider was getting an empty slice which was breaking the provider itself. Because of the pagination I made some changes to client so that it passes all the data it gets from the rollbar api.

--- rollbar/invites.go
+++ rollbar/invites.go
@@ -6,27 +6,31 @@ import (
 	"net/http"
 )
 
-type ListInvitesResponse struct {
-	Error  int `json:"err"`
-	Result []struct {
-		ID           int    `json:"id"`
-		FromUserID   int    `json:"from_user_id"`
-		TeamID       int    `json:"team_id"`
-		ToEmail      string `json:"to_email"`
-		Status       string `json:"status"`
-		DateCreated  int    `json:"date_created"`
-		DateRedeemed int    `json:"date_redeemed"`
-	}
+type ListInvitesResultResponse struct {
+	ID           int    `json:"id"`
+	FromUserID   int    `json:"from_user_id"`
+	TeamID       int    `json:"team_id"`
+	ToEmail      string `json:"to_email"`
+	Status       string `json:"status"`
+	DateCreated  int    `json:"date_created"`
+	DateRedeemed int    `json:"date_redeemed"`
 }
 
-func (c *Client) ListInvites(teamID int) (*ListInvitesResponse, error) {
-	var data ListInvitesResponse
+type ListInvitesResponse struct {
+	Error  int `json:"err"`
+	Result []ListInvitesResultResponse
+}
 
+func (c *Client) ListInvites(teamID int) ([]ListInvitesResultResponse, error) {
+	var invites []ListInvitesResultResponse
 	// Invitation call has pagination.
 	// There's a feature request to expire the invitations after some time.
 	// Looping until we get an empty invitations list [].
 	// Page=0 and page=1 return the same result.
 	for i := 1; ; i++ {
+		var data ListInvitesResponse
+		var dataResponses []ListInvitesResultResponse
+
 		pageNum := i
 		url := fmt.Sprintf("%steam/%d/invites?access_token=%s&page=%d", c.ApiBaseUrl, teamID, c.ApiKey, pageNum)
 		req, err := http.NewRequest("GET", url, nil)
@@ -42,6 +46,8 @@ func (c *Client) ListInvites(teamID int) (*ListInvitesResponse, error) {
 		}
 
 		err = json.Unmarshal(bytes, &data)
+		dataResponses = data.Result
+		invites = append(invites, dataResponses...)
 
 		if err != nil {
 			return nil, err
@@ -51,5 +57,5 @@ func (c *Client) ListInvites(teamID int) (*ListInvitesResponse, error) {
 			break
 		}
 	}
-	return &data, nil
+	return invites, nil
 }
