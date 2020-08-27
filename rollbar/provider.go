75fcd19446296ed33ad2921ca7930fd02da477e4
Jason McVetta <jason.mcvetta@gmail.com>
2020-08-27 20:59:54 +0700

    fix import path

--- rollbar/provider.go
+++ rollbar/provider.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/jmcvetta/terraform-provider-rollbar/client"
+	"github.com/jmcvetta/terraform-provider-rollbar/rollbar/client"
 )
 
 const (
