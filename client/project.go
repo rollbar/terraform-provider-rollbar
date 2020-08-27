d43846caa4a9f9cb75d44bf280c64e1f81c6cf7f
Jason McVetta <jason.mcvetta@gmail.com>
2020-08-27 17:58:42 +0700

    rollbar project data model
    (from API response)

---
+++ client/project.go
@@ -0,0 +1,46 @@
+package client
+
+// Project is a Rollbar project
+type Project struct {
+	AccountId    string `json:"account_id"`
+	DateCreated  int    `json:"date_created"`
+	DateModified int    `json:"date_modified"`
+	Id           int    `json:"id"`
+	Name         string `json:"name"`
+	SettingsData struct {
+		Grouping struct {
+			AutoUpgrade    bool     `json:"auto_upgrade"`
+			RecentVersions []string `json:"recent_versions"`
+		} `json:"grouping"`
+		Integrations struct {
+			Asana       interface{} `json:"asana"`
+			AzureDevops interface{} `json:"azuredevops"`
+			Bitbucket   interface{} `json:"bitbucket"`
+			/*
+				"campfire": {},
+				"ciscospark": {},
+				"clubhouse": {},
+				"datadog": {},
+				"email": {
+					"enabled": true
+				},
+				"flowdock": {},
+				"github": {},
+				"gitlab": {},
+				"hipchat": {},
+				"jira": {},
+				"lightstep": {},
+				"pagerduty": {},
+				"pivotal": {},
+				"slack": {},
+				"sprintly": {},
+				"trello": {},
+				"victorops": {},
+				"webhook": {}
+			*/
+		} `json:"integrations"`
+		TimeFormat string `json:"time_format"`
+		Timezone   string `json:"timezone"`
+	} `json:"settings_data"`
+	Status string `json:"status"`
+}
