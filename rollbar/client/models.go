0f53ca63792b5fa328336a5b883eadabc091b45f
Jason McVetta <jason.mcvetta@gmail.com>
2020-08-28 17:16:18 +0700

    disable model fields that are not yet processed by terraform

--- rollbar/client/models.go
+++ rollbar/client/models.go
@@ -2,47 +2,47 @@ package client
 
 // Project is a Rollbar project
 type Project struct {
-	AccountId    int    `json:"account_id"`
-	DateCreated  int    `json:"date_created"`
-	DateModified int    `json:"date_modified"`
-	Id           int    `json:"id"`
-	Name         string `json:"name"`
-	SettingsData struct {
-		Grouping struct {
-			AutoUpgrade    bool     `json:"auto_upgrade"`
-			RecentVersions []string `json:"recent_versions"`
-		} `json:"grouping"`
-		Integrations struct {
-			Asana       interface{} `json:"asana"`
-			AzureDevops interface{} `json:"azuredevops"`
-			Bitbucket   interface{} `json:"bitbucket"`
-			/*
-				"campfire": {},
-				"ciscospark": {},
-				"clubhouse": {},
-				"datadog": {},
-				"email": {
-					"enabled": true
-				},
-				"flowdock": {},
-				"github": {},
-				"gitlab": {},
-				"hipchat": {},
-				"jira": {},
-				"lightstep": {},
-				"pagerduty": {},
-				"pivotal": {},
-				"slack": {},
-				"sprintly": {},
-				"trello": {},
-				"victorops": {},
-				"webhook": {}
-			*/
-		} `json:"integrations"`
-		TimeFormat string `json:"time_format"`
-		Timezone   string `json:"timezone"`
-	} `json:"settings_data"`
-	Status string `json:"status"`
+	AccountId    int    `json:"account_id" model:"account_id"`
+	DateCreated  int    `json:"date_created" model:"date_created"`
+	DateModified int    `json:"date_modified" model:"date_modified"`
+	Id           int    `json:"id" model:"id"`
+	Name         string `json:"name" model:"name"`
+	//SettingsData struct {
+	//	Grouping struct {
+	//		AutoUpgrade    bool     `json:"auto_upgrade"`
+	//		RecentVersions []string `json:"recent_versions"`
+	//	} `json:"grouping"`
+	//	Integrations struct {
+	//		Asana       interface{} `json:"asana"`
+	//		AzureDevops interface{} `json:"azuredevops"`
+	//		Bitbucket   interface{} `json:"bitbucket"`
+	//		/*
+	//			"campfire": {},
+	//			"ciscospark": {},
+	//			"clubhouse": {},
+	//			"datadog": {},
+	//			"email": {
+	//				"enabled": true
+	//			},
+	//			"flowdock": {},
+	//			"github": {},
+	//			"gitlab": {},
+	//			"hipchat": {},
+	//			"jira": {},
+	//			"lightstep": {},
+	//			"pagerduty": {},
+	//			"pivotal": {},
+	//			"slack": {},
+	//			"sprintly": {},
+	//			"trello": {},
+	//			"victorops": {},
+	//			"webhook": {}
+	//		*/
+	//	} `json:"integrations"`
+	//	TimeFormat string `json:"time_format"`
+	//	Timezone   string `json:"timezone"`
+	//} `json:"settings_data"`
+	Status string `json:"status" model:"status"`
 }
 
 type ListProjectsResult struct {
