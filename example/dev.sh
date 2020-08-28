a9f5fa03867e6ff43e95dae83ab11b1e60e6e269
Jason McVetta <jason.mcvetta@gmail.com>
2020-08-28 17:11:22 +0700

    dev/test cycle script

---
+++ example/dev.sh
@@ -0,0 +1,19 @@
+#===============================================================================
+#
+# Convenience script to build & cleanly install provider; run `terraform apply`
+# on the example Terraform configs; and cat the debug log output.
+#
+#===============================================================================
+
+set -x
+
+clear -x  # Clear the screen but not the scrollback buffer
+
+# Build & cleanly install the latest provider
+(cd .. && make) \
+    && rm -vrf .terraform /tmp/rollbar-terraform.log \
+    && terraform init \
+    && terraform plan 
+
+# Print the debug log
+cat /tmp/rollbar-terraform.log 
