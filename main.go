9445ea06fe6112905c286778a16628c869cb7780
Jason McVetta <jason.mcvetta@gmail.com>
2020-08-28 17:15:42 +0700

    better logging setup, with defered file close

--- main.go
+++ main.go
@@ -4,18 +4,28 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/jmcvetta/terraform-provider-rollbar/rollbar"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"os"
 )
 
 func main() {
+	// Configure logging
+	f, err := os.OpenFile("/tmp/rollbar-terraform.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Panic().
+			Err(err).
+			Msg("Error opening log file")
+	}
+	defer f.Close()
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: f}).
+		With().Caller().Logger()
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	// Start plugin
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return rollbar.Provider()
 		},
 	})
-}
-
-func init() {
-	// Configure logger to display file and line number
-	log.Logger = log.With().Caller().Logger()
 }
