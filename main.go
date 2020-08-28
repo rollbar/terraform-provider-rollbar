6889e6bf2fd241c9f01b348bcbab1461349caef0
Jason McVetta <jason.mcvetta@gmail.com>
2020-08-28 16:24:20 +0700

    enable caller logging in zerolog

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-
 	"github.com/jmcvetta/terraform-provider-rollbar/rollbar"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -15,3 +15,7 @@ func main() {
 	})
 }
 
+func init() {
+	// Configure logger to display file and line number
+	log.Logger = log.With().Caller().Logger()
+}
