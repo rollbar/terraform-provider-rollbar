eb12f618a17e5dd9304a6a31370ea032c6198ef6
Jason McVetta <jason.mcvetta@gmail.com>
2020-10-01 14:42:57 +0700

    activate httpmock before test suite

---
+++ client/client_suite_test.go
@@ -0,0 +1,34 @@
+package client_test
+
+import (
+	"github.com/jarcoal/httpmock"
+	"github.com/jmcvetta/terraform-provider-rollbar/client"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var c *client.RollbarApiClient
+
+func TestClient(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Client Suite")
+}
+
+
+var _ = BeforeSuite(func() {
+	var err error
+	c, err = client.NewClient("fakeTokenString")
+	Expect(err).NotTo(HaveOccurred())
+	httpmock.ActivateNonDefault(c.GetHttpClient())
+})
+
+var _ = BeforeEach(func() {
+	// remove any mocks
+	httpmock.Reset()
+})
+
+var _ = AfterSuite(func() {
+	httpmock.DeactivateAndReset()
+})
