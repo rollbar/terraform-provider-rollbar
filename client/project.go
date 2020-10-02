410be0283fbb16f6d710f9797b2c610327cdf4da
Jason McVetta <jason.mcvetta@gmail.com>
2020-10-02 17:44:26 +0700

    log response body

--- client/project.go
+++ client/project.go
@@ -35,6 +35,8 @@ func (c *RollbarApiClient) ListProjects() ([]Project, error) {
 		return nil, errResp
 	}
 	lpr := resp.Result().(*ProjectListResult)
+	body := string(resp.Body())
+	log.Debug().Str("body", body).Msg("response body")
 
 	// FIXME: After deleting a project through the API, it still shows up in
 	//  the list of projects returned by the API - only with its name set to
