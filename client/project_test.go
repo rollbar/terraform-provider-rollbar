2a54159a8a412f779f2513ff6978194fa4de2003
Jason McVetta <jason.mcvetta@gmail.com>
2020-10-05 13:58:53 +0700

    when list fails it handles the error

--- client/project_test.go
+++ client/project_test.go
@@ -16,36 +16,48 @@ import (
 var errResult = ErrorResult{Err: 500, Message: "Internal Server Error"}
 
 var _ = Describe("Project", func() {
-	It("Lists all projects", func() {
+	When("list succeeds", func() {
 		u := apiUrl + pathProjectList
-		s := fixture("projects/list.json")
-		stringResponse := httpmock.NewStringResponse(200, s)
-		stringResponse.Header.Add("Content-Type", "application/json")
-		responder := httpmock.ResponderFromResponse(stringResponse)
-		httpmock.RegisterResponder("GET", u, responder)
 
-		expected := []Project{
-			{
-				Id:           106671,
-				AccountId:    8608,
-				DateCreated:  1489139046,
-				DateModified: 1549293583,
-				Name:         "Client-Config",
-				Status:       "enabled",
-			},
-			{
-				Id:           12116,
-				AccountId:    8608,
-				DateCreated:  1407933922,
-				DateModified: 1556814300,
-				Name:         "My",
-				Status:       "enabled",
-			},
-		}
-		actual, err := c.ListProjects()
-		Expect(err).NotTo(HaveOccurred())
-		Expect(actual).To(ContainElements(expected))
-		Expect(actual).To(HaveLen(len(expected)))
+		It("lists all projects", func() {
+			s := fixture("projects/list.json")
+			stringResponse := httpmock.NewStringResponse(200, s)
+			stringResponse.Header.Add("Content-Type", "application/json")
+			responder := httpmock.ResponderFromResponse(stringResponse)
+			httpmock.RegisterResponder("GET", u, responder)
+
+			expected := []Project{
+				{
+					Id:           106671,
+					AccountId:    8608,
+					DateCreated:  1489139046,
+					DateModified: 1549293583,
+					Name:         "Client-Config",
+					Status:       "enabled",
+				},
+				{
+					Id:           12116,
+					AccountId:    8608,
+					DateCreated:  1407933922,
+					DateModified: 1556814300,
+					Name:         "My",
+					Status:       "enabled",
+				},
+			}
+			actual, err := c.ListProjects()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(actual).To(ContainElements(expected))
+			Expect(actual).To(HaveLen(len(expected)))
+		})
+
+		When("list fails", func() {
+			responder := httpmock.NewJsonResponderOrPanic(http.StatusInternalServerError, errResult)
+			It("handles the error", func() {
+				httpmock.RegisterResponder("GET", u, responder)
+				_, err := c.ListProjects()
+				Expect(err).To(MatchError(&errResult))
+			})
+		})
 	})
 
 	When("creating a new project", func() {
