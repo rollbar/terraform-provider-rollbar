918d4be7ff17662f22ff628851f941eec83c9401
Jason McVetta <jason.mcvetta@gmail.com>
2020-10-05 12:42:09 +0700

    test error handling on project creation

--- client/project_test.go
+++ client/project_test.go
@@ -1,11 +1,14 @@
 package client
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/brianvoe/gofakeit/v5"
 	"github.com/jarcoal/httpmock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/rs/zerolog/log"
+	"net/http"
 )
 
 var _ = Describe("Project", func() {
@@ -44,26 +47,49 @@ var _ = Describe("Project", func() {
 	When("creating a new project", func() {
 		u := apiUrl + pathProjectCreate
 		name := gofakeit.HackerNoun()
-		s := fmt.Sprintf(`
-		{
-			"err": 0,
-			"result": {
-				"id": 101,
-				"name": "%s",
-				"accountId": 202,
-				"dateCreated": 20200712,
-				"dateModified": 20200712
-			}
-		}
-		`, name)
-		stringResponse := httpmock.NewStringResponse(201, s)
-		stringResponse.Header.Add("Content-Type", "application/json")
-		responder := httpmock.ResponderFromResponse(stringResponse)
-		It("is created correctly", func() {
-			httpmock.RegisterResponder("POST", u, responder)
-			_, err := c.CreateProject(name)
-			Expect(err).NotTo(HaveOccurred())
 
+		Context("and creation succeeds", func() {
+			s := fmt.Sprintf(fixture("projects/create.json"), name)
+			// FIXME: The actual Rollbar API sends http.StatusOK; but it
+			//  _should_ send http.StatusCreated
+			stringResponse := httpmock.NewStringResponse(http.StatusOK, s)
+			stringResponse.Header.Add("Content-Type", "application/json")
+			responder := func(req *http.Request) (*http.Response, error) {
+				//proj := make(map[string]string)
+				proj := Project{}
+				if err := json.NewDecoder(req.Body).Decode(&proj); err != nil {
+					return nil, err
+				}
+				if proj.Name != name {
+					msg := "incorrect name sent to API"
+					log.Error().
+						Str("expected", name).
+						Str("actual", proj.Name).
+						Msg(msg)
+					return nil, fmt.Errorf(msg)
+				}
+				return stringResponse, nil
+			}
+			It("is created correctly", func() {
+				httpmock.RegisterResponder("POST", u, responder)
+				proj, err := c.CreateProject(name)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(proj.Name).To(Equal(name))
+			})
+		})
+
+		Context("and creation fails", func() {
+			s := fixture("projects/create.json")
+			// FIXME: The actual Rollbar API sends http.StatusOK; but it
+			//  _should_ send http.StatusCreated
+			stringResponse := httpmock.NewStringResponse(http.StatusInternalServerError, s)
+			stringResponse.Header.Add("Content-Type", "application/json")
+			responder := httpmock.ResponderFromResponse(stringResponse)
+			It("handles the error cleanly", func() {
+				httpmock.RegisterResponder("POST", u, responder)
+				_, err := c.CreateProject(name)
+				Expect(err).To(MatchError(&ErrorResult{}))
+			})
 		})
 	})
 })
