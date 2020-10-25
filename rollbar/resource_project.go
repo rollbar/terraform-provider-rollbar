fefaca4fa786534f237f8c00125d3519381b632a
Jason McVetta <jason.mcvetta@gmail.com>
2020-10-25 18:02:01 +0700

    delete default tokens on newly created projects

--- rollbar/resource_project.go
+++ rollbar/resource_project.go
@@ -73,39 +73,68 @@ func resourceProject() *schema.Resource {
 }
 
 func resourceProjectCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	var diags diag.Diagnostics
-
 	name := d.Get("name").(string)
-	log.Debug().Str("name", name).
-		Msg("Creating new project")
+	l := log.With().Str("name", name).Logger()
+	l.Info().Msg("Creating new Rollbar project resource")
 
 	c := m.(*client.RollbarApiClient)
 	p, err := c.CreateProject(name)
 	if err != nil {
+		l.Err(err).Send()
 		return diag.FromErr(err)
 	}
-	log.Debug().Interface("project", p).Msg("CreateProject() result")
-
+	l.Debug().Interface("project", p).Msg("CreateProject() result")
+	l = l.With().Int("project_id", p.Id).Logger()
 	d.SetId(strconv.Itoa(p.Id))
 
-	readDiags := resourceProjectRead(ctx, d, m)
-	diags = append(diags, readDiags...)
-	return diags
+	// A set of four default access tokens are automagically created by Rollbar
+	// when creating a new project.  However we only want access tokens that are
+	// explicitly created and managed by Terraform.  Therefore we delete the
+	// default tokens for our new project.
+	expectedTokenNames := map[string]bool{
+		"read":             true,
+		"write":            true,
+		"post_client_item": true,
+		"post_server_item": true,
+	}
+	tokens, err := c.ListProjectAccessTokens(p.Id)
+	if err != nil {
+		l.Err(err).Send()
+		return diag.FromErr(err)
+	}
+	for _, t := range tokens {
+		// Sanity check
+		expected := expectedTokenNames[t.Name]
+		if !expected {
+			err = fmt.Errorf("unexpected token name in default tokens")
+			l.Err(err).Send()
+			return diag.FromErr(err)
+		}
+		// Deletion
+		err = c.DeleteProjectAccessToken(p.Id, t.AccessToken)
+		if err != nil {
+			l.Err(err).Send()
+			return diag.FromErr(err)
+		}
+		l.Debug().
+			Str("name", t.Name).
+			Msg("Successfully deleted a default access token")
+	}
+
+	l.Info().Msg("Successfully created Rollbar project resource")
+	return resourceProjectRead(ctx, d, m)
 }
 
 func resourceProjectRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	var diags diag.Diagnostics
-
 	id, err := strconv.Atoi(d.Id())
 	if err != nil {
 		log.Err(err).Msg("Error converting Id to integer")
 		return diag.FromErr(err)
 	}
-
 	l := log.With().
 		Int("id", id).
 		Logger()
-	l.Debug().Msg("Reading project resource")
+	l.Info().Msg("Reading Rollbar project resource")
 
 	c := m.(*client.RollbarApiClient)
 	proj, err := c.ReadProject(id)
@@ -120,12 +149,13 @@ func resourceProjectRead(ctx context.Context, d *schema.ResourceData, m interfac
 		}}
 	}
 	if err != nil {
+		l.Err(err).Send()
 		return diag.FromErr(err)
 	}
+
 	var mProj map[string]interface{}
 	err = mapstructure.Decode(proj, &mProj)
 	if err != nil {
-		l.Err(err).Send()
 		return diag.FromErr(err)
 	}
 	for k, v := range mProj {
@@ -134,13 +164,13 @@ func resourceProjectRead(ctx context.Context, d *schema.ResourceData, m interfac
 		}
 		err = d.Set(k, v)
 		if err != nil {
-			l.Err(err).Send()
 			return diag.FromErr(err)
 		}
 	}
-	d.SetId(strconv.Itoa(proj.Id))
 
-	return diags
+	d.SetId(strconv.Itoa(proj.Id))
+	l.Info().Msg("Successfully read Rollbar project resource from the API")
+	return nil
 }
 
 /*
@@ -152,24 +182,20 @@ func resourceProjectUpdate(ctx context.Context, d *schema.ResourceData, m interf
 */
 
 func resourceProjectDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	var diags diag.Diagnostics
-
 	projectId, err := strconv.Atoi(d.Id())
 	if err != nil {
-		log.Err(err).Msg("Error converting Id to integer")
 		return diag.FromErr(err)
 	}
-
 	l := log.With().
 		Int("projectId", projectId).
 		Logger()
-	l.Debug().Msg("Deleting project")
-
+	l.Info().Msg("Deleting Rollbar project resource")
 	c := m.(*client.RollbarApiClient)
 	err = c.DeleteProject(projectId)
 	if err != nil {
+		l.Err(err).Send()
 		return diag.FromErr(err)
 	}
-
-	return diags
+	l.Info().Msg("Successfully deleted Rollbar project resource")
+	return nil
 }
