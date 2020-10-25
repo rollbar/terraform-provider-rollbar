f027a10290a8a7defbf22eafa857a280b585398f
Jason McVetta <jason.mcvetta@gmail.com>
2020-10-25 21:01:32 +0700

    TestAccTeam()

--- rollbar/resource_team_test.go
+++ rollbar/resource_team_test.go
@@ -1,1 +1,104 @@
 package rollbar_test
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/rollbar/terraform-provider-rollbar/client"
+	"github.com/rs/zerolog/log"
+	"os"
+	"strings"
+)
+
+func init() {
+	resource.AddTestSweepers("rollbar_team", &resource.Sweeper{
+		Name: "rollbar_team",
+		F:    sweepResourceTeam,
+	})
+}
+
+// TestAccTeam tests CRUD operations for a Rollbar team.
+func (s *AccSuite) TestAccTeam() {
+	rn := "rollbar_team.test"
+
+	resource.Test(s.T(), resource.TestCase{
+		PreCheck: func() { s.preCheck() },
+		//ProviderFactories: testAccProviderFactories(),
+		Providers:    s.providers,
+		CheckDestroy: nil,
+		Steps: []resource.TestStep{
+			{
+				// language=hcl-terraform
+				Config: s.configResourceTeam(),
+				Check: resource.ComposeTestCheckFunc(
+					s.checkResourceStateSanity(rn),
+					resource.TestCheckResourceAttr(rn, "name", s.projectName),
+					s.checkTeam(rn),
+					//s.checkProjectInProjectList(rn),
+				),
+			},
+			{
+				ResourceName:      rn,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func (s *AccSuite) configResourceTeam() string {
+	// langauge=hcl
+	tmpl := `
+		resource "rollbar_team" "test" {
+			name = "%s"
+		}
+	`
+	teamName := fmt.Sprintf("%s-team-0", s.projectName)
+	return fmt.Sprintf(tmpl, teamName)
+
+}
+
+// checkTeam checks that the newly created team exists and has correct
+// attributes.
+func (s *AccSuite) checkTeam(rn string) resource.TestCheckFunc {
+	return func(ts *terraform.State) error {
+		id, err := s.getResourceIDInt(ts, rn)
+		s.Nil(err)
+		c := s.provider.Meta().(*client.RollbarApiClient)
+		t, err := c.ReadTeam(id)
+		s.Nil(err)
+		s.Equal(s.projectName, t.Name, "team name from API does not match team name in Terraform config")
+		s.Equal("standard", t.AccessLevel)
+		return nil
+	}
+}
+
+func sweepResourceTeam(_ string) error {
+	log.Info().Msg("Cleaning up Rollbar teams from acceptance test runs.")
+	token := os.Getenv("ROLLBAR_API_KEY")
+	c := client.NewClient(token)
+
+	teams, err := c.ListTeams()
+	if err != nil {
+		log.Err(err).Send()
+		return err
+	}
+
+	for _, t := range teams {
+		l := log.With().
+			Str("name", t.Name).
+			Int("id", t.ID).
+			Logger()
+		if strings.HasPrefix(t.Name, "tf-acc-test-") {
+			err = c.DeleteTeam(t.ID)
+			if err != nil {
+				l.Err(err).Send()
+				return err
+			}
+			l.Info().Msg("Deleted team")
+		}
+	}
+
+	log.Info().Msg("Teams cleanup complete")
+	return nil
+}
