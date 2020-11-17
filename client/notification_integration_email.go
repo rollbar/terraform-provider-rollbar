5d7758704d654854c0223b0fdd91c284dedcc47d
Jason McVetta <jason.mcvetta@gmail.com>
2020-11-17 17:40:17 +0700

    UpdateNotificationsIntegrationEmail

---
+++ client/notification_integration_email.go
@@ -0,0 +1,32 @@
+package client
+
+import "github.com/rs/zerolog/log"
+
+// UpdateNotificationsIntegrationEmail updates settings for Rollbar notifications
+// email integration.
+func (c *RollbarApiClient) UpdateNotificationsIntegrationEmail(enabled, includeRequestParams bool) error {
+	l := log.With().
+		Bool("enabled", enabled).
+		Bool("include_request_params", includeRequestParams).
+		Logger()
+	l.Debug().Msg("Updating email notification integration")
+
+	u := apiUrl + pathNotificationIntegrationEmail
+	resp, err := c.Resty.R().
+		SetBody(map[string]interface{}{
+			"enabled":                enabled,
+			"include_request_params": includeRequestParams,
+		}).
+		SetError(ErrorResult{}).
+		Put(u)
+	if err != nil {
+		l.Err(err).Msg("Error updating email notification integration")
+		return err
+	}
+	err = errorFromResponse(resp)
+	if err != nil {
+		l.Err(err).Msg("Error updating email notification integration")
+		return err
+	}
+	return nil
+}
