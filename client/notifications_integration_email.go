3130ea5956c93b34c05a09ba18d6aa914c47d87a
Jason McVetta <jason.mcvetta@gmail.com>
2020-11-18 17:42:45 +0700

    EmailIntegrationArgs

--- client/notifications_integration_email.go
+++ client/notifications_integration_email.go
@@ -2,21 +2,28 @@ package client
 
 import "github.com/rs/zerolog/log"
 
+// EmailIntegrationArgs encapsulates the arguments required to update a Rollbar
+// notifications email integration.
+type EmailIntegrationArgs struct {
+	Token                string `json:"-"`
+	Enabled              bool   `json:"enabled"`
+	IncludeRequestParams bool   `json:"include_request_params"`
+}
+
 // UpdateNotificationsEmailIntegration updates settings for Rollbar notifications
 // email integration.
-func (c *RollbarApiClient) UpdateNotificationsEmailIntegration(enabled, includeRequestParams bool) error {
+func (c *RollbarApiClient) UpdateNotificationsEmailIntegration(args EmailIntegrationArgs) error {
 	l := log.With().
-		Bool("enabled", enabled).
-		Bool("include_request_params", includeRequestParams).
+		Interface("args", args).
 		Logger()
 	l.Debug().Msg("Updating email notification integration")
 
 	u := apiUrl + pathNotificationIntegrationEmail
 	resp, err := c.Resty.R().
-		SetBody(map[string]interface{}{
-			"enabled":                enabled,
-			"include_request_params": includeRequestParams,
-		}).
+		// Project access token is required, instead of the account access token
+		// Resty is configured with by default.
+		SetHeader("X-Rollbar-Access-Token", args.Token).
+		SetBody(args).
 		SetError(ErrorResult{}).
 		Put(u)
 	if err != nil {
