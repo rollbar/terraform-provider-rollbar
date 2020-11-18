fa260a9dae39b363e3ce83276dab4fd64b9213a8
Jason McVetta <jason.mcvetta@gmail.com>
2020-11-18 18:05:35 +0700

    TestAccNotificationsEmailIntegrationBadSyntax

--- rollbar/resource_notifications_email_integration_test.go
+++ rollbar/resource_notifications_email_integration_test.go
@@ -6,10 +6,10 @@ import (
 	"regexp"
 )
 
-// TestAccNotificationsEmailIntegrationCreateNotImpelmented tests creating a
+// TestAccNotificationsSlackIntegrationCreateNotImplemented tests creating a
 // Rollbar notifications email integration - it expects an error because this
 // resource's read method is not yet implemented.
-func (s *AccSuite) TestAccNotificationsEmailIntegrationCreateNotImpelmented() {
+func (s *AccSuite) TestAccNotificationsEmailIntegrationCreateNotImplemented() {
 	// language=hcl
 	tmpl := `
 		resource "rollbar_project" "test" {
@@ -40,9 +40,27 @@ func (s *AccSuite) TestAccNotificationsEmailIntegrationCreateNotImpelmented() {
 		},
 	})
 }
-func (s *AccSuite) TestAccNotificationsEmailIntegrationNotImpelmented() {
+
+func (s *AccSuite) TestAccNotificationsEmailIntegrationBadSyntax() {
 	// language=hcl
-	tmpl := `
+	tmplNoEnabled := `
+		resource "rollbar_project" "test" {
+			name = "%s"
+		}
+
+		resource "rollbar_project_access_token" "test" {
+			project_id = rollbar_project.test.id
+			name = "test_%s"
+			scopes = ["write"]
+		}
+
+		resource "rollbar_notifications_email_integration" "singleton" {
+			project_access_token = rollbar_project_access_token.test.access_token
+		}
+	`
+	configNoEnabled := fmt.Sprintf(tmplNoEnabled, s.randName, s.randName)
+	// language=hcl
+	tmplNoToken := `
 		resource "rollbar_project" "test" {
 			name = "%s"
 		}
@@ -55,18 +73,21 @@ func (s *AccSuite) TestAccNotificationsEmailIntegrationNotImpelmented() {
 
 		resource "rollbar_notifications_email_integration" "singleton" {
 			enabled = true
-			project_access_token = rollbar_project_access_token.test.access_token
 		}
 	`
-	config := fmt.Sprintf(tmpl, s.randName, s.randName)
+	configNoToken := fmt.Sprintf(tmplNoToken, s.randName, s.randName)
 	resource.ParallelTest(s.T(), resource.TestCase{
 		PreCheck:     func() { s.preCheck() },
 		Providers:    s.providers,
 		CheckDestroy: nil,
 		Steps: []resource.TestStep{
 			{
-				Config:      config,
-				ExpectError: regexp.MustCompile("not yet implemented"),
+				Config:      configNoEnabled,
+				ExpectError: regexp.MustCompile(`argument "enabled" is required`),
+			},
+			{
+				Config:      configNoToken,
+				ExpectError: regexp.MustCompile(`argument "project_access_token" is required`),
 			},
 		},
 	})
