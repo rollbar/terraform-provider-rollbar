87d9246a1fa6860ed72f77480808395daa97e5db
Jason McVetta <jason.mcvetta@gmail.com>
2020-11-18 17:43:19 +0700

    TestAccNotificationsEmailIntegrationCreateNotImpelmented

--- rollbar/resource_notifications_email_integration_test.go
+++ rollbar/resource_notifications_email_integration_test.go
@@ -1,1 +1,73 @@
 package rollbar
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"regexp"
+)
+
+// TestAccNotificationsEmailIntegrationCreateNotImpelmented tests creating a
+// Rollbar notifications email integration - it expects an error because this
+// resource's read method is not yet implemented.
+func (s *AccSuite) TestAccNotificationsEmailIntegrationCreateNotImpelmented() {
+	// language=hcl
+	tmpl := `
+		resource "rollbar_project" "test" {
+			name = "%s"
+		}
+
+		resource "rollbar_project_access_token" "test" {
+			project_id = rollbar_project.test.id
+			name = "test_%s"
+			scopes = ["write"]
+		}
+
+		resource "rollbar_notifications_email_integration" "singleton" {
+			enabled = true
+			project_access_token = rollbar_project_access_token.test.access_token
+		}
+	`
+	config := fmt.Sprintf(tmpl, s.randName, s.randName)
+	resource.ParallelTest(s.T(), resource.TestCase{
+		PreCheck:     func() { s.preCheck() },
+		Providers:    s.providers,
+		CheckDestroy: nil,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				ExpectError: regexp.MustCompile("not yet implemented"),
+			},
+		},
+	})
+}
+func (s *AccSuite) TestAccNotificationsEmailIntegrationNotImpelmented() {
+	// language=hcl
+	tmpl := `
+		resource "rollbar_project" "test" {
+			name = "%s"
+		}
+
+		resource "rollbar_project_access_token" "test" {
+			project_id = rollbar_project.test.id
+			name = "test_%s"
+			scopes = ["write"]
+		}
+
+		resource "rollbar_notifications_email_integration" "singleton" {
+			enabled = true
+			project_access_token = rollbar_project_access_token.test.access_token
+		}
+	`
+	config := fmt.Sprintf(tmpl, s.randName, s.randName)
+	resource.ParallelTest(s.T(), resource.TestCase{
+		PreCheck:     func() { s.preCheck() },
+		Providers:    s.providers,
+		CheckDestroy: nil,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				ExpectError: regexp.MustCompile("not yet implemented"),
+			},
+		},
+	})
+}
