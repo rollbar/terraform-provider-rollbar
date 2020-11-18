17baf213dc4328769fbbd37eca0dbb010138e932
Jason McVetta <jason.mcvetta@gmail.com>
2020-11-18 17:57:25 +0700

    enable slack integration resource

--- rollbar/provider.go
+++ rollbar/provider.go
@@ -56,6 +56,7 @@ func Provider() *schema.Provider {
 			"rollbar_team":                 resourceTeam(),
 			"rollbar_user":                 resourceUser(),
 			resNameNotificationsEmail:      resourceNotificationsEmailIntegration(),
+			resNameNotificationsSlack:      resourceNotificationsSlackIntegration(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"rollbar_project":               dataSourceProject(),
