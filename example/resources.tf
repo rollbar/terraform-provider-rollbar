af15fd9c8c6363b600c49cad3014f83179e42710
Pawel Szczodruch <pawel.szczodruch@rollbar.com>
2022-02-14 16:32:01 -0800

    fixed the tf test

--- example/resources.tf
+++ example/resources.tf
@@ -43,7 +43,7 @@ resource "rollbar_project_access_token" "test_2" {
   depends_on = [rollbar_project.test]
 }
 
-resource "rollbar_team" "test_team_0" {
+resource "rollbar_team" "test_team_1" {
   name = "test-team-example"
 }
 
