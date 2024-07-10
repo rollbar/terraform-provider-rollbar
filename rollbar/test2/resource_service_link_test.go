d4d53392b77878fb63f1924e40b2481f232a8fa5
Pawel <pawelsz125125-github@yahoo.com>
2024-07-10 12:01:24 -0700

    un-skip tests

--- rollbar/test2/resource_service_link_test.go
+++ rollbar/test2/resource_service_link_test.go
@@ -40,7 +40,6 @@ func init() {
 
 // TestServiceLinkCreate tests creating a service link
 func (s *AccSuite) TestServiceLinkCreate() {
-	s.T().Skip("unauthorized")
 	serviceLinkResourceName := "rollbar_service_link.service_link"
 	// language=hcl
 	tmpl := `
@@ -70,7 +69,6 @@ func (s *AccSuite) TestServiceLinkCreate() {
 
 // TestServiceLinkUpdate tests updating a service link
 func (s *AccSuite) TestServiceLinkUpdate() {
-	s.T().Skip("unauthorized")
 	serviceLinkResourceName := "rollbar_service_link.service_link"
 	// language=hcl
 	tmpl1 := `
